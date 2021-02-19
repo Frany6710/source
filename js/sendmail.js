45e64f8bdefc9c81404c2c7c8b76f9ed67b2de69
Frany6710 <61435815+Frany6710@users.noreply.github.com>
2021-02-19 16:17:35 -0300

    Update sendmail.js

--- js/sendmail.js
+++ js/sendmail.js
@@ -6,8 +6,8 @@ const { Webhook } = require("discord-webhook-node")
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "automaticmail.frany6710@gmail.com",
-        pass: "gatonto18"
+        user: "*******@gmail.com",
+        pass: "*****"
     }
 });
 
@@ -18,10 +18,10 @@ function sendMail(email) {
 
 
     let MailOptions = {
-        from: "Sistema automatico de mails de frany6710 <automaticmail.frany6710@gmail.com>",
+        from: "******* <*******@gmail.com>",
         to: email,
-        subject: "Frany lo contactara pronto",
-        text: "Dentro de 24 horas frany lo contactara"
+        subject: "Test",
+        text: "Test"
     }
 
     // Send
@@ -31,7 +31,7 @@ function sendMail(email) {
             return console.log(err)
         }
         console.log(data)
-        const hook = new Webhook("https://discord.com/api/webhooks/812137564097085492/RmVQqyCNwbEN8Uvv2itlz3eFb7lK6gVA50L1ZregtPXKKLHYDkabhHVc0ko3WyECqBqL")
+        const hook = new Webhook("************")
         hook.send("Envie un mail a: "+email)
     })
 }
